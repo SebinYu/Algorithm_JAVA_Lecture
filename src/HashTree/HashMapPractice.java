c7a8a0231dc18d6f68e9d40c0ed6b4fb0f10081b
SebinYu <sebinyu72@gmail.com>
2022-10-31 23:43:40 +0900

    feat: HashMap 연습

---
+++ src/HashTree/HashMapPractice.java
@@ -0,0 +1,33 @@
+package HashTree;
+
+import java.util.*;
+class HashMapPractice {
+    public char solution(int n, String s){
+        char answer=' ';
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(char x : s.toCharArray()){
+            map.put(x, map.getOrDefault(x, 0)+1);
+        }
+        //System.out.println(map.containsKey('F'));
+        //System.out.println(map.size());
+        //System.out.println(map.remove('C'));
+
+        int max=Integer.MIN_VALUE;
+        for(char key : map.keySet()){
+            //System.out.println(key+" "+map.get(key));
+            if(map.get(key)>max){
+                max=map.get(key);
+                answer=key;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        HashMapPractice T = new HashMapPractice();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        String str=kb.next();
+        System.out.println(T.solution(n, str));
+    }
+}
